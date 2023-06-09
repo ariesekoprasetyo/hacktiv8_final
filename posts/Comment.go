e5c786ed1b3e8775940dbef8020d695c1bfee04f
Aries-PC <ariesekoprasetyo@ydrive.pro>
2023-06-09 19:04:00 +0700

    [ADD] 09062023 1903

--- posts/Comment.go
+++ posts/Comment.go
@@ -1,4 +1,20 @@
 package posts
 
+import (
+	"hacktiv8_final/controller"
+	"hacktiv8_final/repository"
+)
+
 type CommentController struct {
+	commentController controller.Posts
+	CommentRepo       RepositoryComment
+}
+
+func (c *CommentController) CreateComment(request controller.CreateCommentRequest) {
+	comment := repository.Comment{
+		UserId:  request.UserId,
+		PhotoID: request.UserId,
+		Message: request.Message,
+	}
+	c.CommentRepo.Save(comment)
 }
