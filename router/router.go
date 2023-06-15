14b8ca535e53365979643889ba6489c88afa7a76
Aries-PC <ariesekoprasetyo@ydrive.pro>
2023-06-15 19:04:25 +0700

    [ADD] 15062023 1903

--- router/router.go
+++ router/router.go
@@ -11,7 +11,7 @@ import (
 func CORSMiddleware(c *gin.Context) {
 	cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Content-Type", "Content-Length", "Authorization", "Origin"},
 		ExposeHeaders:    []string{"Content-Type", "Content-Length"},
 		AllowCredentials: true,
